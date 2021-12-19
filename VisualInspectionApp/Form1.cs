23be52194f639ad5edb5e5263ae7def75c0ba218
root <yasuda.takayuki.dev@gmail.com>
2021-12-19 23:00:20 +0900

    add 推論のpipeline

--- VisualInspectionApp/Form1.cs
+++ VisualInspectionApp/Form1.cs
@@ -1,5 +1,7 @@
 using System.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 
 namespace VisualInspectionApp
 {
@@ -50,6 +52,31 @@ namespace VisualInspectionApp
             try
             {
                 //TODO: êÑò_èàóùÇèëÇ≠
+                var assetsRelativePath = @"..\..\..\assets";
+                string assetsPath = GetAbsolutePath(assetsRelativePath);
+                var modelFilePath = Path.Combine(assetsPath, "Model", "segmentation_model.onnx");
+                var imagesFolder = Path.Combine(assetsPath, "images");
+                var outputFolder = Path.Combine(assetsPath, "images", "output");
+
+                // Initialize MLContext
+                MLContext mlContext = new MLContext();
+
+                IEnumerable<ImageNetData> images = ImageNetData.ReadFromFile(imagesFolder);
+                IDataView imageDataView = mlContext.Data.LoadFromEnumerable(images);
+
+                // Create IDataView from empty list to obtain input data schema
+                var data = mlContext.Data.LoadFromEnumerable(new List<ImageNetData>());
+
+                // Define scoring pipeline
+                var pipeline = mlContext.Transforms.LoadImages(outputColumnName: "image", imageFolder: "", inputColumnName: nameof(ImageNetData.ImagePath))
+                                .Append(mlContext.Transforms.ApplyOnnxModel(modelFile: modelFilePath, outputColumnNames: new[] { SegmentaionModelSettings.ModelOutput }, inputColumnNames: new[] { SegmentaionModelSettings.ModelInput }));
+
+                //var pipeline = mlContext.Transforms.ApplyOnnxModel(modelFile: modelLocation, outputColumnNames: new[] { SegmentaionModelSettings.ModelOutput }, inputColumnNames: new[] { SegmentaionModelSettings.ModelInput });
+
+                // Fit scoring pipeline
+                var model = pipeline.Fit(data);
+
+                IDataView scoredData = model.Transform(imageDataView);
 
                 var from2 = new Form2();
                 from2.Show();
@@ -60,5 +87,44 @@ namespace VisualInspectionApp
                 MessageBox.Show(ex.ToString());
             }
         }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+    }
+
+    public class ImageNetData
+    {
+        [LoadColumn(0)]
+        public string ImagePath;
+
+        [LoadColumn(1)]
+        public string Label;
+
+        public static IEnumerable<ImageNetData> ReadFromFile(string imageFolder)
+        {
+            return Directory
+                .GetFiles(imageFolder)
+                .Select(filePath => new ImageNetData { ImagePath = filePath, Label = Path.GetFileName(filePath) });
+        }
+    }
+
+    public struct SegmentaionModelSettings
+    {
+        // for checking Tiny yolo2 Model input and  output  parameter names,
+        //you can use tools like Netron, 
+        // which is installed by Visual Studio AI Tools
+
+        // input tensor name
+        public const string ModelInput = "input_1";
+
+        // output tensor name
+        public const string ModelOutput = "sigmoid";
     }
 }
